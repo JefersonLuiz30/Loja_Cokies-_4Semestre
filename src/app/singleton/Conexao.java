71a8823ea145842c2fb8bf01965cb980b683667a
JEFERSON  LUIZ DA SILVA <0220832311038@fatectq.local>
2024-09-24 11:53:22 -0300

    atualizado

---
+++ src/app/singleton/Conexao.java
@@ -0,0 +1,35 @@
+package app.singleton;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class Conexao {
+    private static Conexao instancia;
+    private Connection connection;
+    private String driver = "com.mysql.jdbc.Driver";
+    private String url = "jdbc:mysql://localhost/dbds2imovel";
+    private String usuario = "root";
+    private String senha = "";
+
+    public Connection getConexao() {
+        // Carregar o driver
+        try {
+            Class.forName(driver);
+            this.connection = DriverManager.getConnection(url, usuario, senha);
+            return this.connection;
+        } catch (ClassNotFoundException | SQLException e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+
+    // metodo static vc consegue acesso mesmo que objeto não esteja criado
+    //
+    public static synchronized Conexao getInstancia() {
+        if (instancia == null) {
+            instancia = new Conexao();
+        }
+        return instancia;
+    }
+}
